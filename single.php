5c19f79e3077bf7492c50157300fc24e49263d86
unknown <mohsendroo@yahoo.com>
2023-07-02 18:13:48 +0430

    last commit 1402/04/12

---
+++ single.php
@@ -0,0 +1,163 @@
+<?php include __DIR__ . '/header.php'; ?>
+
+<main id="main" class="main single">
+    <section id="blog-single" class="container blog-single">
+        <div class="row top-content gap g-3">
+            <div class="col-12 col-xl-6 excerpt-column column">
+                
+            </div>
+            <div class="col-12 col-xl-6 thumbnail-column column">
+                
+            </div>
+        </div>
+
+        <div class="row main-content">
+            <div class="col-12 column">
+                
+            </div>
+        </div> 
+
+        <div class="row buttom-content">
+            <div class="col-12 column">
+                
+            </div>
+        </div> 
+
+        <div class="row related">
+            <div class="col-12 column">
+                <!-- Component Heading Start -->
+                <div class="heading">
+                    <div class="title">آخرین اخبار و بلاگ</div>
+                    <div class="link">
+                        <div class="navigation d-none d-sm-block">
+                            <div class="nav next blogswiper-button-next"><i class="las la-angle-right"></i></div>
+                            <div class="nav prev blogswiper-button-prev"><i class="las la-angle-left"></i></div>
+                        </div>
+                        <a href="https://gmaxads.com/youtubeads/" class="more">
+                            <button type="button" class="btn">
+                                <span>مشاهده بیشتر</span>
+                                <i class="las la-paperclip"></i>
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <!-- Component Heading End -->
+            </div>
+
+            <div class="col-12 column">
+                <!-- Swiper -->
+                <div class="swiper blogSwiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <!-- Component Blog Card Start -->
+                            <div class="blog-card">
+                                <div class="info">
+                                    <a href="#" class="thumbnail">
+                                        <img src="assets/img/blog.jpg" alt="thumbnail">
+                                    </a>
+                                    <ul class="detial line-clamp-1">
+                                        <li><a href="#">22 خرداد 1402</a></li>
+                                        <li><a href="#">ملیکا عامری فر</a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <a href="#" class="title line-clamp-1">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</a>
+                                    <p class="desc line-clamp-3">
+                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد 
+                                    </p>
+                                </div>
+                            </div>
+                            <!-- Component Blog Card End -->
+                        </div>
+                        <div class="swiper-slide">
+                            <!-- Component Blog Card Start -->
+                            <div class="blog-card">
+                                <div class="info">
+                                    <a href="#" class="thumbnail">
+                                        <img src="assets/img/blog.jpg" alt="thumbnail">
+                                    </a>
+                                    <ul class="detial line-clamp-1">
+                                        <li><a href="#">22 خرداد 1402</a></li>
+                                        <li><a href="#">ملیکا عامری فر</a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <a href="#" class="title line-clamp-1">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</a>
+                                    <p class="desc line-clamp-3">
+                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد 
+                                    </p>
+                                </div>
+                            </div>
+                            <!-- Component Blog Card End -->
+                        </div>
+                        <div class="swiper-slide">
+                            <!-- Component Blog Card Start -->
+                            <div class="blog-card">
+                                <div class="info">
+                                    <a href="#" class="thumbnail">
+                                        <img src="assets/img/blog.jpg" alt="thumbnail">
+                                    </a>
+                                    <ul class="detial line-clamp-1">
+                                        <li><a href="#">22 خرداد 1402</a></li>
+                                        <li><a href="#">ملیکا عامری فر</a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <a href="#" class="title line-clamp-1">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</a>
+                                    <p class="desc line-clamp-3">
+                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد 
+                                    </p>
+                                </div>
+                            </div>
+                            <!-- Component Blog Card End -->
+                        </div>
+                        <div class="swiper-slide">
+                            <!-- Component Blog Card Start -->
+                            <div class="blog-card">
+                                <div class="info">
+                                    <a href="#" class="thumbnail">
+                                        <img src="assets/img/blog.jpg" alt="thumbnail">
+                                    </a>
+                                    <ul class="detial line-clamp-1">
+                                        <li><a href="#">22 خرداد 1402</a></li>
+                                        <li><a href="#">ملیکا عامری فر</a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <a href="#" class="title line-clamp-1">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</a>
+                                    <p class="desc line-clamp-3">
+                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد 
+                                    </p>
+                                </div>
+                            </div>
+                            <!-- Component Blog Card End -->
+                        </div>
+                        <div class="swiper-slide">
+                            <!-- Component Blog Card Start -->
+                            <div class="blog-card">
+                                <div class="info">
+                                    <a href="#" class="thumbnail">
+                                        <img src="assets/img/blog.jpg" alt="thumbnail">
+                                    </a>
+                                    <ul class="detial line-clamp-1">
+                                        <li><a href="#">22 خرداد 1402</a></li>
+                                        <li><a href="#">ملیکا عامری فر</a></li>
+                                    </ul>
+                                </div>
+                                <div class="content">
+                                    <a href="#" class="title line-clamp-1">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.</a>
+                                    <p class="desc line-clamp-3">
+                                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد 
+                                    </p>
+                                </div>
+                            </div>
+                            <!-- Component Blog Card End -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </section>
+</main>
+
+<?php include __DIR__ . '/footer.php'; ?>
